8d103755b3d501b12cb9db13e354e335c4eb94a2
Jacob Lehmer <jacob.lehmer@inl.gov>
2026-06-25 12:16:29 -0600

    Inital stub for creating the online logic version of the CIE Tool

---
+++ onlineLogic/index.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<h1>Cyber Informed Engineering</h1>
+<h2>To begin a new cyber informed engineering analysis click <a href="/registration.php">Here</a></h2>
+
+<?php
+?>
---
+++ onlineLogic/registration.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<h1>Cyber Informed Engineering</h1>
+<h2>Project Details</h2>
+<label for="projectName">Project Name</label><input id="projectName" type="text" ></input>
+<br>
+<label for="projectDescription">Project Description</label><textarea id="projectDescription"></textarea>
+<br>
+<h2>Location Details</h2>
+<label for="projectAddress">Project Address</label><textarea id="projectAddress"></textarea>
+<br>
+<h2>Contact Details</h2>
+<label for="projectLead">Project Lead</label><input id="projectLead" type="text" ></input>
+<br>
+<label for="projectLeadPhone">Phone</label><input id="projectLeadPhone" type="text" ></input>
+<br>
+<label for="projectLeadEmail">Email</label><input id="projectLeadEmail" type="text" ></input>
+<br>
+<?php
+?>
